7f70ed36f35b99378dd3a4d869e95d54e32a78cd
Tzekov <46439753+stanislavtz@users.noreply.github.com>
2022-05-04 00:42:57 +0300

    Update express configuration

--- src/config/express.js
+++ src/config/express.js
@@ -4,10 +4,16 @@ const path = require('path');
 const env = process.env.NODE_ENV || 'development';
 const port = require('./config')[env].port;
 
-const app = express();
+function configExpress(app) {
+    app.use(express.static(path.resolve(__dirname, '../static')));
 
-app.use(express.static(path.resolve(__dirname, '../static')))
+    app.get('/', (req, res) => {
+        res.render('home');
+    });
+    
+    app.listen(port, console.log.bind(console, `Server is running on http://localhost:${port}`));
+}
 
-app.listen(port, console.log.bind(console, `Server is running on http://localhost:${port}`));
 
-module.exports = app;
+
+module.exports = configExpress;
