6fa331bf6041b33e2ba1d3d9c0c9439a5db1036e
Tzekov <stanislavtz@gmail.com>
2022-05-04 17:09:36 +0300

    Add  render details page functionality

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,19 +1,27 @@
 const router = require('express').Router();
-const { create } = require('../services/cubeService');
+const { create, getById } = require('../services/cubeService');
 
 const renderCreateCubePage = (req, res) => res.render('create');
+const renderCubeDetailsPage = (req, res) => {
+    const id = req.params.id;
+    getById(id)
+        .then(cube => res.render('details', { ...cube }))
+        .catch(err => console.error(err));
+
+}
 
 const createCube = (req, res) => {
     const { name, imageUrl, description, difficulty } = req.body;
     const cube = { name, imageUrl, description, difficulty }
 
     create(cube)
-        .then(result => console.log('Cube created: ', result));    
-    
-    res.redirect('/');
+        .then(res.redirect('/'))
+        .catch(err => console.log(err));
 }
 
 router.get('/cube/create', renderCreateCubePage);
+router.get('/cube/:id/details', renderCubeDetailsPage);
+
 router.post('/cube/create', createCube);
 
 module.exports = router;
--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -2,10 +2,13 @@ const Cube = require("../models/Cube");
 
 const getAll = () => Cube.find().lean();
 
+const getById = (id) => Cube.findById(id).lean();
+
 const create = (cube) => Cube.create(cube);
 
 
 module.exports = {
     create,
     getAll,
+    getById,
 }
