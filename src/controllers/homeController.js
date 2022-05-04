54920c6deb5d9683d50c02593a05491e24c55c68
Tzekov <stanislavtz@gmail.com>
2022-05-04 16:26:27 +0300

    Add search functionality

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -3,13 +3,41 @@ const { getAll } = require('../services/cubeService');
 
 router.get('/', renderHomePage);
 router.get('/about', renderAboutPage);
+router.get('/search', renderSearchResult);
 
-function renderHomePage(req, res) {
-    getAll()
-        .then(cubes => {
-            res.render('home', { cubes });
-        })
-        .catch(err => console.error(err));
+async function renderHomePage(req, res) {
+    try {
+        const cubes = await getAll();
+        res.render('home', { cubes })
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function renderSearchResult(req, res) {
+    let result = await getAll();
+
+    if (req.query.search) {
+        result = result.filter(r =>
+            r.name.toLowerCase().includes(req.query.search.toLowerCase()));
+    }
+
+    if (req.query.from) {
+        result = result.filter(r => Number(r.difficulty) >= Number(req.query.from));
+    }
+
+    if (req.query.to) {
+        result = result.filter(r => Number(r.difficulty) <= Number(req.query.to));
+    }
+
+    console.log('result', result)
+
+    if (result.length > 0) {
+
+        return res.render('home', { cubes: result });
+    }
+
+    return res.redirect('/');
 }
 
 function renderAboutPage(req, res) {
