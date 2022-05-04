a7e17fc5104808f1893e3255cfc209ea27de4dbd
Tzekov <stanislavtz@gmail.com>
2022-05-04 13:22:05 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const dbConnect = require('./config/database');
 
 const env = process.env.NODE_ENV || 'development';
 const port = require('./config/config')[env].port;
 
 const app = express();
 
-require('./config/express')(app);
-require('./config/handlebars')(app);
-
-app.listen(port, console.log.bind(console, `Server is running on http://localhost:${port}`));
+dbConnect()
+    .then(() => {
+        console.log('Database is connected!');
+        require('./config/express')(app);
+        require('./config/handlebars')(app);
+        app.listen(port, console.log.bind(console, `Server is running on http://localhost:${port}`));
+    })
+    .catch(err => console.error(err));
