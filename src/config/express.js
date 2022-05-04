e3446996a18a3452099a06cd31178b5dc17fd3e1
Tzekov <46439753+stanislavtz@users.noreply.github.com>
2022-05-04 08:50:07 +0300

    Refactor index and config modules

--- src/config/express.js
+++ src/config/express.js
@@ -3,17 +3,9 @@ const path = require('path');
 
 const routes = require('../routes');
 
-const env = process.env.NODE_ENV || 'development';
-const port = require('./config')[env].port;
-
 function configExpress(app) {
     app.use(express.static(path.resolve(__dirname, '../static')));
     app.use(routes);
-    
-    
-    app.listen(port, console.log.bind(console, `Server is running on http://localhost:${port}`));
 }
-
-
 
 module.exports = configExpress;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 
+const env = process.env.NODE_ENV || 'development';
+const port = require('./config/config')[env].port;
+
 const app = express();
 
 require('./config/express')(app);
 require('./config/handlebars')(app);
+
+app.listen(port, console.log.bind(console, `Server is running on http://localhost:${port}`));
