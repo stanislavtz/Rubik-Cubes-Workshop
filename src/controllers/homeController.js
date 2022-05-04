20692e6702973be1f7479af1eeebeda6b70d222e
Tzekov <stanislavtz@gmail.com>
2022-05-04 15:20:35 +0300

    Update home page controller

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,10 +1,15 @@
 const router = require('express').Router();
+const { getAll } = require('../services/cubeService');
 
 router.get('/', renderHomePage);
 router.get('/about', renderAboutPage);
 
 function renderHomePage(req, res) {
-    res.render('home');
+    getAll()
+        .then(cubes => {
+            res.render('home', { cubes });
+        })
+        .catch(err => console.error(err));
 }
 
 function renderAboutPage(req, res) {
