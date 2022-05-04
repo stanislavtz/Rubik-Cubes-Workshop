3a2097c9146d4250f514434e2a394d37b478f30f
Tzekov <stanislavtz@gmail.com>
2022-05-04 13:23:00 +0300

    Add urlencoding to express config

--- src/config/express.js
+++ src/config/express.js
@@ -5,6 +5,7 @@ const routes = require('../routes');
 
 function configExpress(app) {
     app.use(express.static(path.resolve(__dirname, '../static')));
+    app.use(express.urlencoded({ extended: true }));
     app.use(routes);
 }
 
