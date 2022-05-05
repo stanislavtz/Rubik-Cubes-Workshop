59c412b2f4166d184984317958c11812143b3ec0
Tzekov <stanislavtz@gmail.com>
2022-05-05 15:24:34 +0300

    Update accessoriyService

--- src/services/accessoryService.js
+++ src/services/accessoryService.js
@@ -2,6 +2,12 @@ const Accessory = require('../models/Accessory');
 
 const create = (accessory) => Accessory.create(accessory);
 
+const getAllAccessories = () => Accessory.find().lean();
+
+const getAccessoryById = (id) => Accessory.findById(id).lean();
+
 module.exports = {
     create,
+    getAllAccessories,
+    getAccessoryById
 }
