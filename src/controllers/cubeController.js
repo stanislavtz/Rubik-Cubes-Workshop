95da06dee64014d6dc4565b4b0707c6396e37538
Tzekov <stanislavtz@gmail.com>
2022-05-05 15:24:11 +0300

    Update renderCubeAtachAccessoryPage functionality

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { getAllAccessories, getAccessoryById } = require('../services/accessoryService');
 const { create, getCubeById } = require('../services/cubeService');
 
 const renderCreateCubePage = (req, res) => res.render('cube/create');
@@ -20,12 +21,32 @@ const createCube = (req, res) => {
         .catch(err => console.log(err));
 }
 
-const renderCubeAttachAccessoryPage = (req, res) => {
-    const id = req.params.id;
+let cube;
+const renderCubeAttachAccessoryPage = async (req, res) => {
+    try {
+        const id = req.params.id;
+        cube = await getCubeById(id);
+        const accessories = await getAllAccessories();
+        const notAttached = accessories.filter(a => !a.cubes.includes(cube._id));
 
-    getCubeById(id)
-        .then(cube => res.render('cube/attach', {...cube}))
-        .catch(err => console.error(err));
+        res.render('cube/attach', { ...cube, accessories: notAttached });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const attachAccessory = async (req, res) => {
+    try {
+        const accessoryId = req.body.accessory;
+        const accessory = await getAccessoryById(accessoryId);
+
+        cube.accessories.push(accessory._id);
+        accessory.cubes.push(cube._id);
+
+        res.redirect(`/`);
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 router.get('/create', renderCreateCubePage);
@@ -33,5 +54,6 @@ router.get('/:id/details', renderCubeDetailsPage);
 router.get('/:id/attach', renderCubeAttachAccessoryPage);
 
 router.post('/create', createCube);
+router.post('/:id/attach', attachAccessory);
 
 module.exports = router;
