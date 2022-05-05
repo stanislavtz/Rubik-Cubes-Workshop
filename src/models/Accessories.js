e6a61177eb904a80c1bf76030bb581d9096d1ef2
Tzekov <stanislavtz@gmail.com>
2022-05-05 10:36:28 +0300

    Update Accessory model

--- src/models/Accessories.js
+++ src/models/Accessories.js
@@ -1,21 +1,22 @@
 const mongoose = require('mongoose');
+const { imageUrlValidator } = require('../utils/validators');
 
 const accessorySchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true,
+        required: [true, 'Name is required'],
     },
     imageUrl: {
         type: String,
-        required: true,
-        validate: [],
+        required: [true, 'imageUrl is required'],
+        validate: imageUrlValidator,
     },
     description: {
         type: String,
-        required: true,
-        maxlength: 200,
+        required: [true, 'Description is required'],
+        maxlength: [200, 'Text length should be maximum 200 characters.'],
     },
-    cubes: []
+    cubes: [{type: mongoose.Types.ObjectId, ref: 'Cube'}]
 });
 
 const Accessory = mongoose.model('Accessory', accessorySchema);
