621884169caaab766bde39f54279dfc9c19c681f
Tzekov <stanislavtz@gmail.com>
2022-05-05 11:20:53 +0300

    Update router

--- src/routes.js
+++ src/routes.js
@@ -4,8 +4,8 @@ const cubeController = require('./controllers/cubeController');
 const accessoryController = require('./controllers/accessoryController');
 
 router.use(homeController);
-router.use(cubeController);
-router.use(accessoryController);
+router.use('/cube', cubeController);
+router.use('/accessory', accessoryController);
 router.get('*', (req, res) => res.render('404'));
 
 module.exports = router;
