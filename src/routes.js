fab452db56803a353a73f3ea862c5ef6886d2a52
Tzekov <stanislavtz@gmail.com>
2022-05-05 11:20:04 +0300

    Add accessoryController middleware

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const homeController = require('./controllers/homeController');
 const cubeController = require('./controllers/cubeController');
+const accessoryController = require('./controllers/accessoryController');
 
 router.use(homeController);
 router.use(cubeController);
+router.use(accessoryController);
 router.get('*', (req, res) => res.render('404'));
 
 module.exports = router;
