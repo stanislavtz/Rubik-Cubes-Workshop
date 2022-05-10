bcedaa8f8f56f2047076173c87777e1900e3fb21
Tzekov <46439753+stanislavtz@users.noreply.github.com>
2022-05-10 20:24:54 +0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ const router = require('express').Router();
 const cubeRouter = require('./routers/cubeRouter');
 const homePageRouter = require('./routers/homePageRouter');
 const accessoryRouter = require('./routers/accessoryRouter');
+const userRouter = require('./routers/userRouter');
 
 router.use(homePageRouter);
 router.use('/cube', cubeRouter);
 router.use('/accessory', accessoryRouter);
+router.use(userRouter);
 
 router.get('*', (req, res) => res.status(404).render('404'));
 
