4fedca29e5ffae2bdb0d2ba8737b52ff73b80014
Tzekov <46439753+stanislavtz@users.noreply.github.com>
2022-05-11 08:39:57 +0300

    Update createUser and add loginUser functionality

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,7 +1,21 @@
 const User = require("../models/User");
+const bcrypt = require('bcrypt');
 
-const createUser = (user) => User.create(user);
+const createUser = ({ username, password, repeatPassword }) => {
+    if (password !== repeatPassword) {
+        throw { message: `Password don't match` }
+    }
+
+    return User.create({ username, password });
+}
+
+const loginUser = async ({ username, password }) => {
+    const user = await User.findOne({ username });
+    console.log('User: ', user);
+    return await bcrypt.compare(password, user.password);
+}
 
 module.exports = {
     createUser,
+    loginUser
 }
