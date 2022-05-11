82460e04c86b969742e2240b4d2866de2418b6ca
Tzekov <46439753+stanislavtz@users.noreply.github.com>
2022-05-12 00:28:43 +0300

    Add AUTH_COOKIE_NAME string

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,9 @@
-const cubiclePass = 'mPWearFFQ1ZbESWn';
+const CUBICLE_DB_PASS = 'mPWearFFQ1ZbESWn';
 
 exports.connectionString = 'mongodb://localhost:27017/cubicle';
 
-exports.cloudConnectionString = `mongodb+srv://stanislavtz:${cubiclePass}@cubicle0.hmntl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+exports.cloudConnectionString = `mongodb+srv://stanislavtz:${CUBICLE_DB_PASS}@cubicle0.hmntl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 exports.SECRET = 'OneTwoThreeComeAndPlayWithMe';
+
+exports.AUTH_COOKIE_NAME = '#user-auth-token';
