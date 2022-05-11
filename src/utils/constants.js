9a4a42e2d85bc386e6d992686e112ddbb97d68bd
Tzekov <stanislavtz@gmail.com>
2022-05-11 13:05:56 +0300

    Add SECRET key

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,5 @@ const cubiclePass = 'mPWearFFQ1ZbESWn';
 exports.connectionString = 'mongodb://localhost:27017/cubicle';
 
 exports.cloudConnectionString = `mongodb+srv://stanislavtz:${cubiclePass}@cubicle0.hmntl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+exports.SECRET = 'OneTwoThreeComeAndPlayWithMe';
