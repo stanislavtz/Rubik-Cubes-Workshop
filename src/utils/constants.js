69e8deb15120ea9e04fdefa1ba246e7e867ec5ab
Tzekov <stanislavtz@gmail.com>
2022-05-11 12:13:51 +0300

    Fix wrong new line in connection string

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,8 +5,7 @@ const cubiclePass = 'mPWearFFQ1ZbESWn';
 
 exports.connectionString = 'mongodb://localhost:27017/cubicle';
 
-exports.cloudConnectionString = `mongodb+srv://stanislavtz:${cubiclePass}@cubicle0.
-    hmntl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+exports.cloudConnectionString = `mongodb+srv://stanislavtz:${cubiclePass}@cubicle0.hmntl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 exports.jwtPromise = {
     sign: util.promisify(jwt.sign),
