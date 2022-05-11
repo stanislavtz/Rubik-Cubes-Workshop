9c85be7c4976ca087d4a4a38ea603e59fc2745f7
Tzekov <46439753+stanislavtz@users.noreply.github.com>
2022-05-11 08:39:22 +0300

    Add user register and user login routes

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -2,10 +2,15 @@ const router = require('express').Router();
 
 const {
     renderLoginPage,
-    renderRegisterPage
+    renderRegisterPage,
+    register,
+    login
 } = require('../controllers/userController');
 
 router.get('/register', renderRegisterPage);
 router.get('/login', renderLoginPage);
+
+router.post('/register', register);
+router.post('/login', login);
 
 module.exports = router;
