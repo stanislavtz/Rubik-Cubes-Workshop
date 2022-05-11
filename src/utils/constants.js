d58162b2e0dbc537d2e26558516aa6e5ee5d24ae
Tzekov <stanislavtz@gmail.com>
2022-05-11 12:11:55 +0300

    Prmisisfy jwt

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,14 @@
+const util = require('util');
+const jwt = require('jsonwebtoken');
+
 const cubiclePass = 'mPWearFFQ1ZbESWn';
 
 exports.connectionString = 'mongodb://localhost:27017/cubicle';
 
-exports.cloudConnectionString = `mongodb+srv://stanislavtz:${cubiclePass}@cubicle0.hmntl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+exports.cloudConnectionString = `mongodb+srv://stanislavtz:${cubiclePass}@cubicle0.
+    hmntl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+exports.jwtPromise = {
+    sign: util.promisify(jwt.sign),
+    verify: util.promisify(jwt.verify)
+}
