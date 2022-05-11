9a78818cc42c487a4db8c3fbe7e6e505efa93cc0
Tzekov <stanislavtz@gmail.com>
2022-05-11 12:11:20 +0300

    Add cookie-parser and jwt

--- src/config/express.js
+++ src/config/express.js
@@ -1,11 +1,13 @@
 const express = require('express');
-
 const path = require('path');
+const cookieParser = require('cookie-parser');
+
 const routes = require('../routes');
 
 function configExpress(app) {
     app.use(express.static(path.resolve(__dirname, '../static')));
     app.use(express.urlencoded({ extended: true }));
+    app.use(cookieParser());
     app.use(routes);
 }
 
