adc3c5be3bb6b0f4c2161d04e256d34901f157d5
Tzekov <46439753+stanislavtz@users.noreply.github.com>
2022-05-22 10:56:22 +0300

    Update user login functionality

--- src/services/userService.js
+++ src/services/userService.js
@@ -14,22 +14,25 @@ const createUser = ({ username, password, repeatPassword }) => {
 const loginUser = async ({ username, password }) => {
     try {
         const user = await User.findOne({ username });
-        const isLogged = await bcrypt.compare(password, user.password);
-        
-        if (isLogged) {
-            const payload = {
-                _id: user._id,
-                username: user.username
-            }
-
-            const options = {
-                expiresIn: '1d'
-            }
-
-            return jwtPromise.sign(payload, SECRET, options);
+        if(!user) {
+            throw {message: 'Wrong username or password'}
         }
 
-        throw {message: 'Wrong username or password'}
+        const isLogged = await bcrypt.compare(password, user.password);
+        if(!isLogged) {
+            throw {message: 'Wrong username or password'}
+        }
+
+        const payload = {
+            _id: user._id,
+            username: user.username
+        }
+
+        const options = {
+            expiresIn: '1d'
+        }
+
+        return jwtPromise.sign(payload, SECRET, options);
         
     } catch (err) {
         throw err;
